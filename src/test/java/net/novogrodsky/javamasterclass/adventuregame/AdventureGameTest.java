8f2ee0ebe8dda1e48089c300ea9b905bd9789250
Unknown <david.novogrodsky@gmail.com>
2018-06-17 15:25:14 -0500

    adding location files and new classes
    Adding the new location files and code to read the locations and exits from files instead of static blocks.

---
+++ src/test/java/net/novogrodsky/javamasterclass/adventuregame/AdventureGameTest.java
@@ -0,0 +1,32 @@
+package net.novogrodsky.javamasterclass.adventuregame;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class AdventureGameTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  @DisplayName("Test for main()")
+  void main() {
+    assertTrue(true);
+  }
+
+  @Test
+  @Disabled("Not implemented yet")
+  void testShowSomething() {
+  }
+}
