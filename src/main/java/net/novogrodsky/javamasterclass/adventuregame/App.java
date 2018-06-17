a1ce3e3974bfb67e7a3d8df740d7f5bc2756d42d
David Novogrodsky <david.novogrodsky@gmail.com>
2018-06-17 14:37:07 -0500

    initial commit

---
+++ src/main/java/net/novogrodsky/javamasterclass/adventuregame/App.java
@@ -0,0 +1,13 @@
+package net.novogrodsky.javamasterclass.adventuregame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
